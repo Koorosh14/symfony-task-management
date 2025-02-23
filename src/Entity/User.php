ce44899b8b7902629a6b180a51e50c3b4e36b791
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-02-23 11:52:34 +0330

    Add User entity and migration

---
+++ src/Entity/User.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\UserRole;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class User implements PasswordAuthenticatedUserInterface
+{
+	#[ORM\Id]
+	#[ORM\GeneratedValue]
+	#[ORM\Column]
+	private ?int $id = null;
+
+	#[ORM\Column(length: 180)]
+	private ?string $email = null;
+
+	/**
+	 * @var string The hashed password
+	 */
+	#[ORM\Column]
+	private ?string $password = null;
+
+	#[ORM\Column(length: 255, nullable: true)]
+	private ?string $name = null;
+
+	#[ORM\Column(enumType: UserRole::class)]
+	private ?UserRole $role = UserRole::USER;
+
+	#[ORM\Column(options: ['default' => false])]
+	private ?bool $isActive = null;
+
+	#[ORM\Column(type: 'datetime')]
+	private ?\DateTime $createdAt = null;
+
+	#[ORM\Column(type: 'datetime')]
+	private ?\DateTime $updatedAt = null;
+
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
+
+	public function getEmail(): ?string
+	{
+		return $this->email;
+	}
+
+	public function setEmail(string $email): static
+	{
+		$this->email = $email;
+
+		return $this;
+	}
+
+	/**
+	 * A visual identifier that represents this user.
+	 *
+	 * @see UserInterface
+	 */
+	public function getUserIdentifier(): string
+	{
+		return (string) $this->email;
+	}
+
+	/**
+	 * @see PasswordAuthenticatedUserInterface
+	 */
+	public function getPassword(): ?string
+	{
+		return $this->password;
+	}
+
+	public function setPassword(string $password): static
+	{
+		$this->password = $password;
+
+		return $this;
+	}
+
+	public function getName(): ?string
+	{
+		return $this->name;
+	}
+
+	public function setName(?string $name): static
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	public function getRole(): ?UserRole
+	{
+		return $this->role;
+	}
+
+	public function setRole(UserRole $role): static
+	{
+		$this->role = $role;
+
+		return $this;
+	}
+
+	public function isActive(): ?bool
+	{
+		return $this->isActive;
+	}
+
+	public function setIsActive(bool $isActive): static
+	{
+		$this->isActive = $isActive;
+
+		return $this;
+	}
+
+	public function getCreatedAt(): ?\DateTime
+	{
+		return $this->createdAt;
+	}
+
+	public function setCreatedAt(\DateTime $createdAt): static
+	{
+		$this->createdAt = $createdAt;
+
+		return $this;
+	}
+
+	public function getUpdatedAt(): ?\DateTime
+	{
+		return $this->updatedAt;
+	}
+
+	public function setUpdatedAt(\DateTime $updatedAt): static
+	{
+		$this->updatedAt = $updatedAt;
+
+		return $this;
+	}
+
+	/**
+	 * @see UserInterface
+	 */
+	public function eraseCredentials(): void
+	{
+		// If you store any temporary, sensitive data on the user, clear it here
+		// $this->plainPassword = null;
+	}
+}
