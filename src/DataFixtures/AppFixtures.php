9a9e2f282f316ddb2e5d7943e36067eae926af60
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-03-01 15:15:43 +0330

    Add some dummy data to AppFixtures class

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Log;
+use App\Entity\Task;
+use App\Entity\User;
+use App\Enum\TaskStatus;
+use App\Enum\UserRole;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +14,35 @@ class AppFixtures extends Fixture
 {
 	public function load(ObjectManager $manager): void
 	{
-		// $product = new Product();
-		// $manager->persist($product);
+		$user = new User();
+		$user->setEmail('Admin@Test.com');
+		$user->setPassword('12345');
+		$user->setName('Admin');
+		$user->setRole(UserRole::ADMIN);
+		$user->setIsActive(true);
+
+		$manager->persist($user);
+
+		$log = new Log();
+		$log->setAction('user_created');
+		$log->setUserId($user);
+
+		$manager->persist($log);
+
+		$task = new Task();
+		$task->setTitle('Test Task');
+		$task->setDescription('Test Description');
+		$task->setStatus(TaskStatus::IN_PROGRESS);
+		$task->setCreatedBy($user);
+
+		$manager->persist($task);
+
+		$log = new Log();
+		$log->setAction('task_created');
+		$log->setUserId($user);
+		$log->setTaskId($task);
+
+		$manager->persist($log);
 
 		$manager->flush();
 	}
