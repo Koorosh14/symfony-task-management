762990b05e84b3017323e45c18a6e165df23cb7d
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-03-13 21:36:30 +0330

    Update TaskType's buildForm()

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Task;
 use App\Entity\User;
+use App\Enum\TaskStatus;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,25 +18,20 @@ class TaskType extends AbstractType
 		$builder
 			->add('title')
 			->add('description')
-			->add('status')
+			->add('status', ChoiceType::class, [
+				'choices' => [
+					'Pending' => TaskStatus::PENDING,
+					'In Progress' => TaskStatus::IN_PROGRESS,
+					'Completed' => TaskStatus::COMPLETED,
+				],
+			])
 			->add('isImportant')
 			->add('dueDate', null, [
 				'widget' => 'single_text',
 			])
-			->add('parentId')
-			->add('createdAt', null, [
-				'widget' => 'single_text',
-			])
-			->add('updatedAt', null, [
-				'widget' => 'single_text',
-			])
-			->add('createdBy', EntityType::class, [
-				'class' => User::class,
-				'choice_label' => 'id',
-			])
 			->add('assignedTo', EntityType::class, [
 				'class' => User::class,
-				'choice_label' => 'id',
+				'choice_label' => 'name',
 				'multiple' => true,
 			]);
 	}
