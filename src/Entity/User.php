618e3f30aa5f2b5138b3b8787f53055e4ee9d0b9
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-04-01 21:40:22 +0330

    Add a sample register functionality using 'make:registration-form' generator command

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,11 +7,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
 	#[ORM\Id]
@@ -38,7 +40,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 	private ?UserRole $role = UserRole::USER;
 
 	#[ORM\Column(options: ['default' => true])]
-	private ?bool $isActive = null;
+	private ?bool $isActive = true;
 
 	#[ORM\Column]
 	private ?\DateTime $createdAt = null;
